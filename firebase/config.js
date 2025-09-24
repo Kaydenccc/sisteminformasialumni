700d80275d93ac5ef829a4a40d373719bf7f66f1
unknown <ffadilah682@gmail.com>
2025-09-24 21:46:08 +0800

    add

--- firebase/config.js
+++ firebase/config.js
@@ -3,19 +3,24 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 
+// Firebase configuration yang baru
 const firebaseConfig = {
-  apiKey: "AIzaSyDlun-H5HyuDscr1NMIK_O9NT-9LLfU3cs",
-  authDomain: "alumnimtsn2tanatoraja.firebaseapp.com",
-  projectId: "alumnimtsn2tanatoraja",
-  storageBucket: "alumnimtsn2tanatoraja.firebasestorage.app",
-  messagingSenderId: "1049084155371",
-  appId: "1:1049084155371:web:af45d9eef46957696d35ae",
-  measurementId: "G-PC089WSHGQ",
+  apiKey: "AIzaSyAyM-O9jH0y18ojFklyNgGtWhl1hoBoVwE",
+  authDomain: "alumni-mtsn2tanatoraja.firebaseapp.com",
+  projectId: "alumni-mtsn2tanatoraja",
+  storageBucket: "alumni-mtsn2tanatoraja.firebasestorage.app",
+  messagingSenderId: "5662266115",
+  appId: "1:5662266115:web:920b9509c6cee3354c0f36",
+  measurementId: "G-Z9RZFXJTN7",
 };
 
+// Inisialisasi Firebase app
 const app = initializeApp(firebaseConfig);
 
+// Menyiapkan Firebase services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const analytics = getAnalytics(app); // Menambahkan analytics
